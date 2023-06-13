2cdd3d6395368fb206c51e41dc5c51260a9cd641
bilalkeskinosf <bilal.keskin@osf.digital>
2023-06-14 02:33:41 +0300

    generate random quote

--- pages/quotes.vue
+++ pages/quotes.vue
@@ -1,12 +1,13 @@
 <template>
 	<div>
-		<div v-for="quote in quotes" :key="quotes._id">
-			{{ quote }}
-		</div>
+		<button @click="generateRandomQuote">Generate</button>
+		<div>{{ randomQuote.dialog }}</div>
 	</div>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue';
+
 const runtimeConfig = useRuntimeConfig();
 const { baseURL, apiSecret } = runtimeConfig.public;
 
@@ -26,8 +27,40 @@ const useQuotesStore = defineStore({
 	},
 });
 
-const store = useQuotesStore();
-useAsyncData(store.fetchQuotes);
+const useBooksStore = defineStore({
+	id: 'books',
+	state: () => ({
+		books: [],
+	}),
+	actions: {
+		async fetchBooks() {
+			const response = await fetch(`${baseURL}/book`, {
+				headers: { Authorization: `Bearer ${apiSecret}` },
+			});
+			const data = await response.json();
+			this.books = data.docs;
+		},
+	},
+});
 
-const quotes = computed(() => store.quotes);
+const quoteStore = useQuotesStore();
+const bookStore = useBooksStore();
+
+useAsyncData(quoteStore.fetchQuotes);
+useAsyncData(bookStore.fetchBooks);
+
+const quotes = computed(() => quoteStore.quotes);
+const books = computed(() => bookStore.books);
+const randomQuote = ref('');
+
+const generateRandomQuote = () => {
+	const randomIndex = Math.floor(Math.random() * quotes.value.length);
+	randomQuote.value = quotes.value[randomIndex];
+};
+
+const bookName = computed(() => {
+	if (!randomQuote.value) return '';
+	const book = books.value.find((b) => b._id === randomQuote.value.book);
+	return book ? book.name : 'Unknown Book';
+});
 </script>
